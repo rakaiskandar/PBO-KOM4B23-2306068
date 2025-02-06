2ea5b75f47edbe3b11449cc068a981d810491bd9
rakaiskandar <iskandarraka4@gmail.com>
2025-02-06 11:04:20 +0700

    feat: add tugas 1

---
+++ TP1/src/tugaspertemuan1/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspertemuan1;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hallo Word Raka Iskandar dan 2306068 Saya Senang Belajar Java");
+    }
+    
+}
